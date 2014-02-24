471b7247131de0bc55da1a3141dd9b76fffdec09
Nicolas "Norswap" Laurent <norswap@gmail.com>
2014-02-24 11:55:40 +0100

    Change Ubuntu Mono name so that emacs can find it. Great tip for the future: use the fc-match program.

--- plugins/norswap-unix.el
+++ plugins/norswap-unix.el
@@ -1,3 +1,7 @@
 ;; Allows copying from emacs towards the X system.
 (if (eq window-system 'x) (setq x-select-enable-clipboard t))
+
+;; Font
+(set-face-attribute 'default nil :font "Ubuntu Mono 10")
+
 (provide 'norswap-unix)
