6849411c13ba5bd6b0ef5d3de4e65ed9ed94619c
Nicolas "Norswap" Laurent <norswap@gmail.com>
2014-10-22 14:22:02 +0200

    elpa update

---
+++ elpa/lua-mode-20140628.1505/lua-mode-autoloads.el
@@ -0,0 +1,34 @@
+;;; lua-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (lua-mode) "lua-mode" "../../../../Users/norswap/.emacs.d/elpa/lua-mode-20140628.1505/lua-mode.el"
+;;;;;;  "91809190ba4eefbbd73a5d4f850ba580")
+;;; Generated autoloads from ../../../../Users/norswap/.emacs.d/elpa/lua-mode-20140628.1505/lua-mode.el
+
+(autoload 'lua-mode "lua-mode" "\
+Major mode for editing Lua code.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../Users/norswap/.emacs.d/elpa/lua-mode-20140628.1505/lua-mode-pkg.el"
+;;;;;;  "../../../../Users/norswap/.emacs.d/elpa/lua-mode-20140628.1505/lua-mode.el")
+;;;;;;  (21505 52227 316000 0))
+
+;;;***
+
+(provide 'lua-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lua-mode-autoloads.el ends here
